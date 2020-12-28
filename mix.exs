f2a8d9931f01bed4c8556fdece8be5a2850b37a1
James Harton <james@harton.nz>
2020-12-28 20:30:24 +1300

    chore: allow ex_doc to be used in test env.

--- mix.exs
+++ mix.exs
@@ -49,9 +49,9 @@ defmodule Heap.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev},
-      {:earmark, ">= 0.0.0", only: :dev},
-      {:credo, "~> 1.0", only: ~w(dev test)a, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: ~w[dev test]a},
+      {:earmark, ">= 0.0.0", only: ~w[dev test]a},
+      {:credo, "~> 1.0", only: ~w[dev test]a, runtime: false},
       {:git_ops, "~> 2.3", only: ~w[dev test]a, runtime: false}
     ]
   end
