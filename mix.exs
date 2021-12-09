6cdcc44c9ba959685f45649637ed58b35f37d8b9
James Harton <james@harton.nz>
2021-12-09 13:28:08 +1300

    chore(deps): update dependency earmark to >= 0.2.1

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Heap.Mixfile do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: ~w[dev test]a},
-      {:earmark, ">= 0.0.0", only: ~w[dev test]a},
+      {:earmark, ">= 0.2.1", only: ~w[dev test]a},
       {:credo, "~> 1.6", only: ~w[dev test]a, runtime: false},
       {:git_ops, "~> 2.3", only: ~w[dev test]a, runtime: false}
     ]
