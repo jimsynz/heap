ff7ea26e0520e313bb0e98c8f8188af02fa198f7
James Harton <james@harton.nz>
2021-12-09 16:21:03 +1300

    chore(deps): update dependency git_ops to ~> 2.4

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule Heap.Mixfile do
       {:ex_doc, ">= 0.26.0", only: ~w[dev test]a},
       {:earmark, ">= 0.2.1", only: ~w[dev test]a},
       {:credo, "~> 1.6", only: ~w[dev test]a, runtime: false},
-      {:git_ops, "~> 2.3", only: ~w[dev test]a, runtime: false}
+      {:git_ops, "~> 2.4", only: ~w[dev test]a, runtime: false}
     ]
   end
 end
