38164058aff0f85618b4978d4bea55684874eb0a
James Harton <james@harton.nz>
2022-02-21 11:24:41 +1300

    chore(deps): update dependency ex_doc to >= 0.28.1

--- mix.exs
+++ mix.exs
@@ -49,8 +49,8 @@ defmodule Heap.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, ">= 0.26.0", only: ~w[dev test]a},
-      {:earmark, ">= 1.4.20", only: ~w[dev test]a},
+      {:ex_doc, "~> 0.28", only: ~w[dev test]a},
+      {:earmark, "~> 1.4", only: ~w[dev test]a},
       {:credo, "~> 1.6", only: ~w[dev test]a, runtime: false},
       {:git_ops, "~> 2.4", only: ~w[dev test]a, runtime: false}
     ]
